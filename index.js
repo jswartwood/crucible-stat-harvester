444af63cedf3fba044db4b84c101bbcccb4d2e37
Jacob Swartwood <jswartwood@apple.com>
2018-09-21 12:53:37 -0400

    Saving some temp work.

--- index.js
+++ index.js
@@ -97,72 +97,77 @@ const headerRow = [
                     await fs.writeFile(cacheFile, JSON.stringify(matchInfo), 'utf8');
                 }
 
-                let timestamp = matchInfo.period;
-                let activity = matchInfo.activityDetails.readable.activityTypeName;
-                let map = matchInfo.activityDetails.readable.mapName;
+                try {
+                    let timestamp = matchInfo.period;
+                    let activity = matchInfo.activityDetails.readable.activityTypeName;
+                    let map = matchInfo.activityDetails.readable.mapName;
 
-                let playerEntry = matchInfo.entries.find(entry => name === entry.player.destinyUserInfo.displayName);
+                    let playerEntry = matchInfo.entries.find(entry => id === entry.player.destinyUserInfo.membershipId);
 
-                let playerTeam = playerEntry.values.team.basic.displayValue;
+                    let playerTeam = playerEntry.values.team && playerEntry.values.team.basic.displayValue;
 
-                let fireTeamSize = matchInfo.entries.reduce((size, entry) => {
-                    if (playerEntry.values.fireteamId.basic.value === entry.values.fireteamId.basic.value) {
-                        size++;
-                    }
+                    let fireTeamSize = matchInfo.entries.reduce((size, entry) => {
+                        if (playerEntry.values.fireteamId.basic.value === entry.values.fireteamId.basic.value) {
+                            size++;
+                        }
 
-                    return size;
-                }, 0);
+                        return size;
+                    }, 0);
 
-                let fireteamMembers = matchInfo.entries.reduce((members, entry) => {
-                    if (playerEntry.values.fireteamId.basic.value === entry.values.fireteamId.basic.value) {
-                        members.push(entry.player.destinyUserInfo.displayName);
-                    }
+                    let fireteamMembers = matchInfo.entries.reduce((members, entry) => {
+                        if (playerEntry.values.fireteamId.basic.value === entry.values.fireteamId.basic.value) {
+                            members.push(entry.player.destinyUserInfo.displayName);
+                        }
 
-                    return members;
-                }, []);
+                        return members;
+                    }, []);
 
-                let playerTeamStanding = playerEntry.values.standing.basic.displayValue;
-                let playerTeamScore = playerEntry.values.teamScore.basic.value;
+                    let playerTeamStanding = playerEntry.values.standing.basic.displayValue;
+                    let playerTeamScore = playerEntry.values.teamScore.basic.value;
 
-                let enemyTeamScore;
-                try { // Sometimes this fails; perhaps if teams disconnect before match start???
-                    enemyTeamScore = matchInfo.teams.find(team => playerTeam !== team.teamName).score.basic.value;
-                } catch (err) {
-                    try { // Sometimes this works if we've failed above; have no guesses for this one.
-                        enemyTeamScore = matchInfo.entries.find(entry => playerTeam !== entry.values.team.basic.displayValue).score.basic.value;
+                    let enemyTeamScore;
+                    try { // Sometimes this fails; perhaps if teams disconnect before match start???
+                        enemyTeamScore = matchInfo.teams.find(team => playerTeam !== team.teamName).score.basic.value;
                     } catch (err) {
-                        // If we still can't find the score, just zero it.
-                        enemyTeamScore = 0;
+                        try { // Sometimes this works if we've failed above; have no guesses for this one.
+                            enemyTeamScore = matchInfo.entries.find(entry => playerTeam !== entry.values.team.basic.displayValue).score.basic.value;
+                        } catch (err) {
+                            // If we still can't find the score, just zero it.
+                            enemyTeamScore = 0;
+                        }
                     }
+
+                    let score = playerEntry.values.score.basic.value;
+                    let kills = playerEntry.values.kills.basic.value;
+                    let assists = playerEntry.values.assists.basic.value;
+                    let deaths = playerEntry.values.deaths.basic.value;
+                    let completed = playerEntry.values.completed.basic.displayValue;
+
+                    let row = [
+                        matchId,
+                        timestamp,
+                        activity,
+                        map,
+                        name,
+                        fireTeamSize,
+                        fireteamMembers.sort().join('+'),
+                        playerTeam,
+                        playerTeamStanding,
+                        playerTeamScore,
+                        enemyTeamScore,
+                        score,
+                        kills,
+                        assists,
+                        deaths,
+                        completed,
+                    ].join(',') + '\n';
+
+                    await fs.appendFile(`./data/out/${name}.csv`, row, 'utf8');
+                    await fs.appendFile(`./data/out/__clan__.csv`, row, 'utf8');
+                } catch (err) {
+                    console.error(`Error processing match: ${matchId}`, err);
+                    // console.error(`Match info for: ${matchId}`, JSON.stringify(matchInfo));
                 }
-
-                let score = playerEntry.values.score.basic.value;
-                let kills = playerEntry.values.kills.basic.value;
-                let assists = playerEntry.values.assists.basic.value;
-                let deaths = playerEntry.values.deaths.basic.value;
-                let completed = playerEntry.values.completed.basic.displayValue;
-
-                let row = [
-                    matchId,
-                    timestamp,
-                    activity,
-                    map,
-                    name,
-                    fireTeamSize,
-                    fireteamMembers.sort().join('+'),
-                    playerTeam,
-                    playerTeamStanding,
-                    playerTeamScore,
-                    enemyTeamScore,
-                    score,
-                    kills,
-                    assists,
-                    deaths,
-                    completed,
-                ].join(',') + '\n';
-
-                await fs.appendFile(`./data/out/${name}.csv`, row, 'utf8');
-                await fs.appendFile(`./data/out/__clan__.csv`, row, 'utf8');
 
                 bar.tick();
             }
